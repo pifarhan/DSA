44352cbd729bb082d0b28d0cd3b304556c3ea94d
Farhan Khan <farhan3.14159@gmail.com>
2026-07-04 16:05:26 +0530

    Time: 6 ms (99.50%) | Memory: 47.9 MB (58.48%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left =1;
+        int right = 0;
+        for(int pile:piles) {
+            right = Math.max(right,pile);
+
+        }
+        int ans = right;
+
+        while(left<=right) {
+            int mid = left +(right-left)/2;
+            if(Canfinish(piles,h,mid)){
+                ans = mid;
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return ans;
+
+    }
+
+        private static boolean Canfinish(int[]piles,int h,int speed) {
+            long hours =0;
+            for(int pile:piles) {
+                 hours += (pile+speed-1)/speed;
+            }
+            if(hours>h) {
+                return false;
+            }
+            
+        
+        return true;
+        
+        
+    }
+}
