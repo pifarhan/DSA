c60092469dc35955e2447cb3d323bc4c9e65768c
Farhan Khan <farhan3.14159@gmail.com>
2026-07-09 18:48:34 +0530

    Time: 16 ms (21.03%) | Memory: 46.5 MB (25.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
         HashMap <Character, Integer> map = new HashMap<>();
         for(int i=0; i<s.length();i++) {
             Character ch = s.charAt(i);
