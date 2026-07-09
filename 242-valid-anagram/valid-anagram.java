51567b33abbd57bc4c35cb04f71a92b1c7c11d94
Farhan Khan <farhan3.14159@gmail.com>
2026-07-09 18:47:36 +0530

    Time: 16 ms (21.03%) | Memory: 46.4 MB (42.04%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap <Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++) {
+            Character ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(int i=0; i<t.length();i++) {
+            Character ch = t.charAt(i);
+            if(map.get(ch)!=null) {
+                if(map.get(ch) ==1) {
+                    map.remove(ch);
+                } else{
+                    map.put(ch, map.get(ch)-1);
+                }      
+           } else{
+            return false;
+           }
+        
+    }
+
+    return map.isEmpty();
+
+    }
+
+}
