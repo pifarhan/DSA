5f363601794130fc501fa54246089f01b4e961c0
Farhan Khan <farhan3.14159@gmail.com>
2026-07-23 01:16:15 +0530

    Time: 63 ms (23.33%) | Memory: 49.9 MB (22.12%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -27,8 +27,8 @@ class Solution {
             char ch = s.charAt(i);
 
             if ("aeiouAEIOU".indexOf(ch) != -1) {
-                sb.append(vowels.get(index++));
-                
+                sb.append(vowels.get(index));
+                index++;
             } else {
                 sb.append(ch);
             }
